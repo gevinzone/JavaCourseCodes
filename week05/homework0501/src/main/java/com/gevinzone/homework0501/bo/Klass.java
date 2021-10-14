c8b28a4df8e4f02987fbfcf6037dbb8a3aa67750
gevin <flyhigher139@gmail.com>
2021-10-14 10:50:45 +0800

    inject squadLeader

--- week05/homework0501/src/main/java/com/gevinzone/homework0501/bo/Klass.java
+++ week05/homework0501/src/main/java/com/gevinzone/homework0501/bo/Klass.java
@@ -5,16 +5,23 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
 
+import javax.annotation.Resource;
 import java.util.ArrayList;
 import java.util.List;
 
 @Data
 @NoArgsConstructor
-@AllArgsConstructor
 @ToString
 public class Klass {
     private String name;
+    @Resource(name = "student01")
+    private Student squadLeader;
     private List<Student> students = new ArrayList<>(10);
+
+    public  Klass(String name, List<Student> students) {
+        this.name = name;
+        this.students = students;
+    }
 
     public void addStudent(Student student) {
         students.add(student);
